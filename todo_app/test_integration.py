47034ce25ac368eac479987a47ae8c1be2a57602
-loper-rdomanico <-loper-rdomanico@uk.kworld.kpmg.com>
2022-08-01 14:16:21 +0100

    integration test working - only 1 assertion

--- todo_app/test_integration.py
+++ todo_app/test_integration.py
@@ -1,7 +1,17 @@
+import os
 import pytest
-from dotenv import load_dotenv, find_dotenv
-from todo_app import app
+import pymongo
 import mongomock
+from todo_app import app
+from dotenv import load_dotenv, find_dotenv
+
+
+file_path = find_dotenv(".env.test")
+load_dotenv(file_path, override=True)
+
+connection_string_test = os.environ.get('CONNECTION_STRING_TEST')
+client = pymongo.MongoClient(connection_string_test) 
+
 
 @pytest.fixture
 def client():
@@ -11,3 +21,8 @@ def client():
         test_app = app.create_app()
         with test_app.test_client() as client:
             yield client
+    
+def test_index_page(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    
